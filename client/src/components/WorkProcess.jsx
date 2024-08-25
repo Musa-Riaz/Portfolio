a84755b02efb854eff0eb18052e5e018c261aa4e
Musariaz <riazmusa69@gmail.com>
2024-08-26 00:20:16 +0500

    Workspace section

--- client/src/components/WorkProcess.jsx
+++ client/src/components/WorkProcess.jsx
@@ -1,26 +1,67 @@
-import React from 'react'
-import WorkSpaceCards from './WorkSpaceCards'
+import React from "react";
+import WorkSpaceCards from "./WorkSpaceCards";
 import { BiNotepad } from "react-icons/bi";
+import { GoPencil } from "react-icons/go";
+import { SlGraph } from "react-icons/sl";
+import { CiLaptop } from "react-icons/ci";
 
 const WorkProcess = () => {
   return (
-    <div className='bg-[#F0F1F3]'>
-      
-      <div className='w-[1320px] h-[600px] bg-[#F0F1F3] flex gap-4 items-center '>
-            <div >
-                <h1 className='text-4xl font-myfont'>Work Process</h1>
-                <p className='text-slate-700 text-lg mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente laboriosam delectus odio dolor eos.<br/> Reprehenderit eveniet deserunt libero quis odio sint magnam sequi. Aperiam architecto amet doloremque eius, error in?</p>
-                <p className='text-slate-700 text-lg mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, amet officiis! Nisi harum quod quos iste,<br/> rerum molestias! Repudiandae unde sapiente fuga error optio quia numquam, fugit consequatur quis similique!</p>
-            </div>
+    <div className="bg-[#F0F1F3] ">
+      <div className=" w-[1400px] h-[650px] bg-[#F0F1F3] flex  justify-evenly  ">
+        <div className="p-32">
+          <h1 className="text-6xl font-myfont">Work Process</h1>
+          <p className="text-slate-700 text-lg mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            laboriosam delectus odio dolor eos.
+            <br /> Reprehenderit eveniet deserunt libero quis odio sint magnam
+            sequi. Aperiam architecto amet doloremque eius, error in?
+          </p>
+          <p className="text-slate-700 text-lg mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, amet
+            officiis! Nisi harum quod quos iste,
+            <br /> rerum molestias! Repudiandae unde sapiente fuga error optio
+            quia numquam, fugit consequatur quis similique!
+          </p>
+        </div>
 
-            <div>
-                <div><WorkSpaceCards  icon={<BiNotepad className='text-2xl text-white  '/>} title={'Analyze'} >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, totam?</WorkSpaceCards></div>
-                <div>card2</div>
-            </div>
+        <div className="w-[648px] h-[600px] flex gap-8 ">
+          <div className="mt-5 flex flex-col gap-8">
+            <WorkSpaceCards
+              icon={<BiNotepad className="text-2xl text-white  " />}
+              title={"Analyze"}
+            >
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
+              totam?
+            </WorkSpaceCards>
+            <WorkSpaceCards
+              icon={<GoPencil className="text-2xl text-white  " />}
+              title={"Action"}
+            >
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
+              totam?
+            </WorkSpaceCards>
+          </div>
+          <div className="mt-10 flex flex-col gap-8">
+            <WorkSpaceCards
+              icon={<SlGraph className="text-2xl text-white  " />}
+              title={"Action"}
+            >
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
+              totam?
+            </WorkSpaceCards>
+            <WorkSpaceCards
+              icon={<CiLaptop className="text-2xl text-white  " />}
+              title={"Action"}
+            >
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
+              totam?
+            </WorkSpaceCards>
+          </div>
+        </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default WorkProcess
+export default WorkProcess;
--- client/src/components/WorkSpaceCards.jsx
+++ client/src/components/WorkSpaceCards.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const WorkSpaceCards = ({icon, title, children}) => {
   return (
-    <div className='w-[312px] h-[276px] rounded-xl bg-white shadow-lg'>
+    <div className='w-[312px] h-[276px] rounded-xl bg-white shadow-lg  hover:shadow-2xl transform transition-transform duration-300 hover:-translate-y-2 hover:scale-105'>
 
       <div className='p-8'>
       <div className='h-[50px] w-[50px] rounded-lg bg-[#A53DFF] flex items-center justify-center'>{icon}</div>
